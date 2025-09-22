39b888a354ebe23e4e31213a166d11a6c516c960
Sviievmla <164814429+Sviievmla@users.noreply.github.com>
2025-09-22 16:26:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,6 @@ def uploaded_file(filename):
 
 if __name__ == '__main__':
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
